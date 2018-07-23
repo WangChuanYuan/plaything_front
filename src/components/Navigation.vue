4806a61079ccb749566456ba04455d3fb22bbce5
161250135 <161250135@smail.nju.edu.cn>
2018-07-23 17:13:56 +0800

    主页

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,26 @@
 <template>
-  <el-menu :default-active="activeIndex" background-color="#545c64" mode="horizontal" active-text-color="#ffd04b" @select="handleSelect">
-    <el-menu-item index="1">首页</el-menu-item>
-    <el-menu-item index="2">社区</el-menu-item>
-    <el-menu-item index="3">一期一会</el-menu-item>
-    <el-menu-item index="4">杂货</el-menu-item>
-  </el-menu>
+  <div>
+    <el-menu :default-active="activeIndex" background-color="#ffffff" mode="horizontal" active-text-color="#000000"
+             @select="handleSelect">
+      <span id="logo">玩意儿</span>
+      <el-menu-item index="1" style="margin-left: 150px; font-size: large">首页</el-menu-item>
+      <el-menu-item index="2" style="margin-left: 50px; font-size: large">社区</el-menu-item>
+      <el-menu-item index="3" style="margin-left: 50px; font-size: large">一期一会</el-menu-item>
+      <el-menu-item index="4" style="margin-left: 50px; font-size: large">杂货</el-menu-item>
+      <div class="login" v-if="usr">
+        <el-submenu index="5">
+          <template slot="title">
+            <img id="usrImg" v-bind:src="imgURL"/>
+          </template>
+          <el-menu-item index="5-1" style="font-size: large">我的信息</el-menu-item>
+          <el-menu-item index="5-2" style="font-size: large">登出</el-menu-item>
+        </el-submenu>
+      </div>
+      <div class="login" v-else>
+        <el-menu-item index="6" style="font-size: large">登录</el-menu-item>
+      </div>
+    </el-menu>
+  </div>
 </template>
 
 <script>
@@ -12,17 +28,60 @@
     name: "Navigation",
     data() {
       return {
-        activeIndex: '1'
+        activeIndex: '1',
+        usr: "1"
       };
     },
     methods: {
       handleSelect(key, keyPath) {
-        console.log(key, keyPath);
+        switch (key){
+          case "1":
+            window.location.href = "./";
+            this.activeIndex = "1";
+            break;
+          case "2":
+            window.location.href = "./";
+            this.activeIndex = "2";
+            break;
+          case "3":
+            window.location.href = "./";
+            this.activeIndex = "3";
+            break;
+          case "4":
+            window.location.href = "./";
+            this.activeIndex = "4";
+            break;
+          case "5-1":
+            window.location.href = "./";
+            this.activeIndex = "5-1";
+            break;
+          case "5-2":
+            window.location.href = "./register.html";
+            this.activeIndex = "5-2";
+            break;
+          case "6":
+            window.location.href = "./";
+            this.activeIndex = "6";
+            break;
+        }
       }
     }
   }
 </script>
 
 <style scoped>
-
+  #logo {
+    float: left;
+    font-size: 50px;
+    font-family: FZShuTi;
+    margin-left: 150px;
+    margin-top: 3px;
+    color: #ff1627;
+  }
+  .login {
+    float: right;
+  }
+  #usrImg {
+    border-radius: 120px;
+  }
 </style>
--- src/modules/index/index.vue
+++ src/modules/index/index.vue
@@ -1,15 +1,75 @@
 <template>
-  <Navigation></Navigation>
+  <div>
+    <Navigation></Navigation>
+    <el-carousel trigger="click" height="600px" interval="5000">
+      <el-carousel-item v-for="item in imgs" :key="item">
+        <img :src="item" style="width: 100%; height: inherit"/>
+      </el-carousel-item>
+    </el-carousel>
+    <hr/>
+    <div style="font-weight: 900; font-size: 20px">
+    <p class="content">您可以用我们的网站做什么</p>
+    </div>
+    <el-row :gutter="3">
+      <div v-for="service in services">
+        <el-col :span = "3" :offset="2">
+          <div>
+            <img :src="service.imgURL" class="serviceImg"/>
+            <p class="content">{{service.content}}</p>
+          </div>
+        </el-col>
+      </div>
+    </el-row>
+  </div>
 </template>
 
 <script>
-    import Navigation from "../../components/Navigation";
-    export default {
-      name: "index",
-      components: {Navigation}
+  import Navigation from "../../components/Navigation";
+
+  export default {
+    name: "index",
+    components: {Navigation},
+    data() {
+      return {
+        imgs: [
+          require('../../assets/banner1.jpg'),
+          require('../../assets/banner2.jpg'),
+          require('../../assets/banner3.jpg')
+        ],
+        services: [
+          {
+            "imgURL": require('../../assets/service1.jpg'),
+            "content": "分享优质文章，记录你的所见所闻"
+          },
+          {
+            "imgURL": require('../../assets/service2.jpg'),
+            "content": "了解你所不知道的奇闻轶事"
+          },
+          {
+            "imgURL": require('../../assets/service3.jpg'),
+            "content": "留下你生活的足迹"
+          },
+          {
+            "imgURL": require('../../assets/service4.jpg'),
+            "content": "定制专属于你的纪念品"
+          },
+        ]
+      }
     }
+  }
 </script>
 
 <style scoped>
-
+  .serviceImg {
+    width: 200px;
+    height: 200px;
+    border-radius: 200px;
+  }
+  .content {
+    font-weight: bold;
+    text-align: center;
+  }
+  hr {
+    size: 10px;
+  }
 </style>
