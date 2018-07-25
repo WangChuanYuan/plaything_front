7be42fb132e06b4b0f315a28d42f9c519c79d602
161250135 <161250135@smail.nju.edu.cn>
2018-07-25 16:36:39 +0800

    注册页面完成

--- src/modules/register/register.vue
+++ src/modules/register/register.vue
@@ -4,47 +4,79 @@
       <el-header>
         <Navigation></Navigation>
       </el-header>
-      <el-steps :active="step" simple>
-        <el-step icon="el-icon-edit" title="填写个人信息"></el-step>
-        <el-step icon="el-icon-menu" title="选择适合你的个性标签"></el-step>
-      </el-steps>
-      <!--第一步-->
-      <div v-show="step == 0">
-        <el-steps class="verticalSteps" direction="vertical">
-          <el-step icon="el-icon-upload" status="process" title="步骤一" description="上传用户头像，可选择默认"></el-step>
-          <el-step icon="el-icon-document" status="process" title="步骤二" description="完善个人信息"></el-step>
-          <el-step icon="el-icon-star-off" status="process" title="步骤三" description="输入密码"></el-step>
-          <el-step icon="el-icon-star-on" status="process" title="步骤四" description="确认密码"></el-step>
+      <el-form ref="registerForm" :model="registerForm" :rules="rules">
+        <el-steps :active="step" simple>
+          <el-step icon="el-icon-edit" title="填写个人信息"></el-step>
+          <el-step icon="el-icon-menu" title="选择适合你的个性标签"></el-step>
         </el-steps>
-        <div>
-          <div id="uploader">
+        <!--第一步-->
+        <div v-show="step == 0">
+          <el-steps class="verticalSteps" :space="170" direction="vertical">
+            <el-step icon="el-icon-upload" status="process" title="步骤一" description="上传用户头像，可选择默认"></el-step>
+            <el-step icon="el-icon-document" status="process" title="步骤二" description="完善个人信息"></el-step>
+            <el-step icon="el-icon-star-off" status="process" title="步骤三" description="输入密码"></el-step>
+            <el-step icon="el-icon-star-on" status="process" title="步骤四" description="确认密码"></el-step>
+          </el-steps>
+          <el-form-item id="uploader">
             <!--上传头像，此处只是添加，最后注册时上传至服务器 multiple 表示是否支持多选文件-->
-            <el-upload class="avatar-uploader" action="mock" :multiple="false" :auto-upload="false" :on-change="addDisplay">
+            <el-upload class="avatar-uploader" action="mock" :multiple="false" :auto-upload="false"
+                       :on-change="addDisplay">
               <img v-if="displayURL" :src="displayURL" class="avatar">
               <i v-else class="el-icon-plus avatar-uploader-table"></i>
             </el-upload>
+          </el-form-item>
+          <div id="info">
+            <el-form-item prop="userName">
+              <label class="necessary">*</label>
+              <label>用户名</label>
+              <el-input v-model="registerForm.userName" class="input"></el-input>
+            </el-form-item>
+            <el-form-item prop="location">
+              <label style="padding-left: 1.8em">地址</label>
+              <el-input v-model="registerForm.location" class="input"></el-input>
+            </el-form-item>
+            <el-form-item prop="mail">
+              <label style="padding-left: 1.8em">邮箱</label>
+              <el-input v-model="registerForm.mail" class="input"></el-input>
+            </el-form-item>
+            <el-form-item prop="phone">
+              <label>联系方式</label>
+              <el-input v-model="registerForm.phone" class="input"></el-input>
+            </el-form-item>
+          </div>
+          <div id="password">
+            <el-form-item prop="password">
+              <label style="padding-left: 1.5em" class="necessary">*</label>
+              <label>密码</label>
+              <el-input type="password" v-model="registerForm.password" class="input"></el-input>
+            </el-form-item>
+            <el-form-item prop="checkPassword">
+              <label class="necessary">*</label>
+              <label>确认密码</label>
+              <el-input type="password" v-model="registerForm.checkPassword" class="input"></el-input>
+            </el-form-item>
+          </div>
+          <div>
+            <el-button class="rightButton" circle icon="el-icon-arrow-right" @click="next">下一项</el-button>
           </div>
         </div>
-        <div>
-          <el-button class="rightButton" circle icon="el-icon-arrow-right" @click="next">下一项</el-button>
+        <!--第二步-->
+        <div v-show="step == 1">
+          <tag-cloud id="tagCloud" :config="option" :data="hotTag" @clickTag="clickTagItem"></tag-cloud>
+          <div class="tag">
+            <p>您为自己选择的标签有：</p>
+            <el-tag style="margin: 5px;" size="medium" :key="tag" v-for="tag in registerForm.tags" closable
+                    :disable-transitions=false
+                    @close="removeTag(tag)">
+              {{tag}}
+            </el-tag>
+          </div>
+          <div>
+            <el-button class="leftButton" circle icon="el-icon-arrow-left" @click="last">上一项</el-button>
+            <el-button class="rightButton" circle icon="el-icon-check" @click="register('registerForm')">确定</el-button>
+          </div>
         </div>
-      </div>
-      <!--第二步-->
-      <div v-show="step == 1">
-        <tag-cloud id="tagCloud" :config="option" :data="hotTag" @clickTag="clickTagItem"></tag-cloud>
-        <div class="tag">
-          <p>您为自己选择的标签有：</p>
-          <el-tag style="margin: 5px;" size="medium" :key="tag" v-for="tag in registerForm.tags" closable
-                  :disable-transitions=false
-                  @close="removeTag(tag)">
-            {{tag}}
-          </el-tag>
-        </div>
-        <div>
-          <el-button class="leftButton" circle icon="el-icon-arrow-left" @click="last">上一项</el-button>
-          <el-button class="rightButton" circle icon="el-icon-check" @click="register">确定</el-button>
-        </div>
-      </div>
+      </el-form>
       <Footer id="specialFooter"></Footer>
     </el-container>
   </div>
@@ -53,22 +85,49 @@
 <script>
   import Navigation from "../../components/Navigation";
   import Footer from "../../components/Footer";
+  import $ from 'jquery';
 
   export default {
     name: "register",
     components: {Footer, Navigation},
     data: function () {
+      var passwordRule = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.registerForm.checkPassword !== '') {
+            this.$refs.registerForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var checkPasswordRule = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.registerForm.password) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
       return {
         registerForm: {
           userName: "",
           password: "",
+          checkPassword: "",
           avatar: null,
           location: "",
           mail: "",
           phone: "",
           tags: []
         },
-        displayURL: null,
+        rules: {
+          userName: [{required: true, message: "请输入用户名", trigger: 'blur'}],
+          mail: [{type: 'email', message: '请正确输入邮箱地址', trigger: 'blur'}],
+          password: [{required: true, validator: passwordRule, trigger: 'blur'}],
+          checkPassword: [{required: true, validator: checkPasswordRule, trigger: 'blur'}]
+        },
+        displayURL: require('../../assets/defaultDisplay.jpg'),
         hotTag: [
           {"name": "美食"},
           {"name": "人文"},
@@ -91,7 +150,35 @@
       }
     },
     methods: {
-      addDisplay(file, fileList){
+      register(formName) {
+        console.log(this.registerForm);
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            $.ajax({
+              url: '/api/register',
+              type: 'post',
+              data: this.registerForm,
+              success: function (data) {
+                if (data == 'SUCCESS') {
+                  this.$message.success('注册成功');
+                  sessionStorage.setItem('user', JSON.stringify(this.registerForm.userName));
+                  window.location.href = '/';
+                }
+                else {
+                  this.$message.error("用户名已被注册")
+                }
+              },
+              error: function (error) {
+                this.$message.error("错误")
+              }
+            })
+          } else {
+            this.$message.error('填写信息错误，请检查');
+            return false;
+          }
+        });
+      },
+      addDisplay(file, fileList) {
         this.displayURL = file.url;
         this.registerForm.avatar = file.raw;
       },
@@ -118,6 +205,18 @@
   #uploader {
     left: 40%;
     top: 150px;
+    position: absolute;
+  }
+
+  #info {
+    left: 35%;
+    top: 300px;
+    position: absolute;
+  }
+
+  #password {
+    left: 35%;
+    top: 580px;
     position: absolute;
   }
 
@@ -148,6 +247,14 @@
     height: 120px;
     border-radius: 120px;
     display: block;
+  }
+
+  .necessary {
+    color: red;
+  }
+
+  .input {
+    width: 270px;
   }
 
   .verticalSteps {
