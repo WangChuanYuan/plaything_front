c2b031cabf44378e71ee44ea57883d97cb40ac6c
161250135 <161250135@smail.nju.edu.cn>
2018-07-31 19:47:06 +0800

    修改一下

--- src/modules/index/index.js
+++ src/modules/index/index.js
@@ -3,12 +3,10 @@ import Index from './index.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import Talk from '../../components/Talk';
-
 Vue.use(ElementUI)
 
 new Vue({
   el: '#index',
-  components: { Talk },
-  template: '<Talk/>'
+  components: { Index },
+  template: '<Index/>'
 })
